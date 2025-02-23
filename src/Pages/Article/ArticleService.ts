3de548e676181885b650b38ce5f24fcba3eb2033
Denta Muhajir <dentamuhajir@gmail.com>
2025-02-23 15:59:46 +0700

    Set env variable in the root of project

--- src/Pages/Article/ArticleService.ts
+++ src/Pages/Article/ArticleService.ts
@@ -1,12 +1,10 @@
 import axios from "axios"
 import { Article } from "../../Models/Article"
 
-const baseUrl:string = "https://jsonplaceholder.typicode.com" 
-
 const articleService = {
     
     fetchListArticle: async (): Promise<Article[]> => {
-        const endpoint: string  = baseUrl + '/users'
+        const endpoint: string  = import.meta.env.VITE_API_BASE_URL + '/users'
         const response = await axios.get<Article[]>(endpoint)
         return response.data
     },
