fe2c6b7d6b8d2ec28f5ce8b5d8c285c62315d797
Denta Muhajir <dentamuhajir@gmail.com>
2025-03-22 02:30:51 +0700

    Add dummy data product list

---
+++ src/Data/ProductData.tsx
@@ -0,0 +1,16 @@
+export const productList = [
+    {
+        id: 1,
+        name: "iPhone 11 Pro Max",
+        category: "Electronic",
+        description: "This is luxury phone by apple",
+        price: 1349
+    },
+    {
+        id: 2,
+        name: "Samsung Galaxy Fold",
+        category: "Electronic",
+        description: "This is luxury phone by samsung",
+        price: 1699
+    }
+]
