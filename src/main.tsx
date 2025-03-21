a2c0341aa8eb3fafa22c762ed82261befe1be7ad
Denta Muhajir <dentamuhajir@gmail.com>
2025-03-21 22:21:17 +0700

    Configure redux in main

--- src/main.tsx
+++ src/main.tsx
@@ -3,9 +3,17 @@ import { createRoot } from 'react-dom/client'
 import './index.css'
 import { RouterProvider } from 'react-router'
 import { router } from './Routes/Routes.tsx'
+import { Provider } from 'react-redux'
+import { configureStore } from '@reduxjs/toolkit'
+
+const store = configureStore({
+  reducer: {}
+})
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <RouterProvider router={router}/>
+    <Provider store={store}>
+      <RouterProvider router={router}/>
+    </Provider>
   </StrictMode>,
 )
