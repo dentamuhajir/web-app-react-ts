726ec12d355aa370cffcbe7640af693a1c7e3149
Denta Muhajir <dentamuhajir@gmail.com>
2025-03-23 17:16:02 +0700

    Implement reducer listProducts

--- src/main.tsx
+++ src/main.tsx
@@ -5,9 +5,12 @@ import { RouterProvider } from 'react-router'
 import { router } from './Routes/Routes.tsx'
 import { Provider } from 'react-redux'
 import { configureStore } from '@reduxjs/toolkit'
+import ProductReducer from './Reducers/ProductReducer'
 
 const store = configureStore({
-  reducer: {}
+  reducer: {
+    products: ProductReducer
+  }
 })
 
 createRoot(document.getElementById('root')!).render(
