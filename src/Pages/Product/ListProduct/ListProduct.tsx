30be8fe13612f7c1aa970172292b6e893971d017
Denta Muhajir <dentamuhajir@gmail.com>
2025-03-26 00:51:34 +0700

    Install and implement react helmet

--- src/Pages/Product/ListProduct/ListProduct.tsx
+++ src/Pages/Product/ListProduct/ListProduct.tsx
@@ -1,41 +1,45 @@
 import React from 'react'
-import { useSelector } from 'react-redux';
-
+import { useSelector } from 'react-redux'
+import { Helmet } from 'react-helmet'
 type Props = {}
 
 const ListProduct = (props: Props) => {
   const products = useSelector((state) => state.products);
-  console.log(products)
+  console.log(products.length)
   return (
     <>
-      <div>ListProduct</div>
-      
-
-    <table class="table table-sm">
-      <thead>
-        <tr>
-          <th scope="col">#</th>
-          <th scope="col">Name</th>
-          <th scope="col">Categoy</th>
-          <th scope="col">Price</th>
-        </tr>
-      </thead>
-      <tbody>
-      {products.map((item, index) => (
+      <Helmet>
+        <title>List of products</title>
+        <meta name="description" content="List of the product" />
+      </Helmet>
+      <div>List of Product</div>
+      <table className="table table-sm">
+        <thead>
           <tr>
-            <th scope="row">{ item.id }</th>
-            <td>{ item.name }</td>
-            <td>{ item.category }</td>
-            <td>$ { item.price }</td>
+            <th scope="col">#</th>
+            <th scope="col">Name</th>
+            <th scope="col">Categoy</th>
+            <th scope="col">Price</th>
+            <th scope="col">Action</th>
           </tr>
-      ))}
-        
-
-      </tbody>
-    </table>
+        </thead>
+        <tbody>
+        {products.map((item, index) => (
+            <tr>
+              <th scope="row">{ index + 1 }</th>
+              <td>{ item.name }</td>
+              <td>{ item.category }</td>
+              <td>$ { item.price }</td>
+              <td>
+                <button type="button"  className="btn btn-warning me-2">Edit</button>
+                <button type="button" className="btn btn-info">Delete</button>
+              </td>
+            </tr>
+        ))}
+        </tbody>
+      </table>
     </>
-  )
-  
+  )  
 }
 
 export default ListProduct
--- src/main.tsx
+++ src/main.tsx
@@ -4,18 +4,11 @@ import './index.css'
 import { RouterProvider } from 'react-router'
 import { router } from './Routes/Routes.tsx'
 import { Provider } from 'react-redux'
-import { configureStore } from '@reduxjs/toolkit'
-import ProductReducer from './Reducers/ProductReducer'
-
-const store = configureStore({
-  reducer: {
-    products: ProductReducer
-  }
-})
+import Store from './Store/Store.tsx'
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <Provider store={store}>
+    <Provider store={Store}>
       <RouterProvider router={router}/>
     </Provider>
   </StrictMode>,
